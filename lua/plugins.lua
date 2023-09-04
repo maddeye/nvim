be9803245aa9b529898eb7f9f6bb444dd27ff6b3
Daniel Igel <danieligel@mathema.de>
2023-09-04 13:44:16 +0200

    migratet to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,84 +1,69 @@
--- Ensure packer ist installed
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
--- Automatically install on save
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use 'nvim-lua/plenary.nvim'
+local plugins = {
+  'nvim-lua/plenary.nvim',
 
   -- Colorscheme
-  use 'EdenEast/nightfox.nvim'
+  'EdenEast/nightfox.nvim',
 
   -- LSP
-  use {
+  {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
-  }
+  },
 
-  use 'jose-elias-alvarez/typescript.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'jay-babu/mason-null-ls.nvim'
+  'jose-elias-alvarez/typescript.nvim',
+  'jose-elias-alvarez/null-ls.nvim',
+  'jay-babu/mason-null-ls.nvim',
 
   -- Treesitter
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/nvim-treesitter-context'
+  'nvim-treesitter/nvim-treesitter',
+  'nvim-treesitter/nvim-treesitter-context',
 
   -- Text
-  use 'windwp/nvim-autopairs'
-  use 'mg979/vim-visual-multi'
-  use 'gelguy/wilder.nvim'
-  use 'tpope/vim-surround'
-  use({
+  'windwp/nvim-autopairs',
+  'mg979/vim-visual-multi',
+  'gelguy/wilder.nvim',
+  'tpope/vim-surround',
+  ({
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
     end,
-  })
+  }),
 
   -- Lualine
-  use({
+  "kyazdani42/nvim-web-devicons",
+  ({
     "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons", opt = true },
-  })
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+  }),
 
   -- Teleskop
-  use({
+  ({
     "nvim-telescope/telescope.nvim",
     tag = "0.1.0",
-    requires = { { "nvim-lua/plenary.nvim" } },
-  })
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+  }),
 
   -- tmux & split window navigation
-  use("szw/vim-maximizer") -- maximizes and restores current window
+  ("szw/vim-maximizer"), -- maximizes and restores current window
 
   -- git
-  use 'tpope/vim-fugitive'
-  use({
+  'tpope/vim-fugitive',
+  ({
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({
@@ -90,42 +75,32 @@ return require('packer').startup(function(use)
         },
       })
     end,
-  })
+  }),
 
   -- LSP again
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'onsails/lspkind-nvim'
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'onsails/lspkind-nvim',
 
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
 
-  use 'windwp/nvim-ts-autotag'
-  use 'p00f/nvim-ts-rainbow'
-  use 'axelvc/template-string.nvim'
+  'windwp/nvim-ts-autotag',
+  'p00f/nvim-ts-rainbow',
+  'axelvc/template-string.nvim',
 
   -- Style
-  use { 'j-hui/fidget.nvim', branch = 'legacy' }
+  { 'j-hui/fidget.nvim',     branch = 'legacy' },
 
   -- Move in visual mode
-  use { 'echasnovski/mini.move', branch = 'stable' }
+  { 'echasnovski/mini.move', branch = 'stable' },
 
   -- Typescript helper
-  use 'marilari88/twoslash-queries.nvim'
+  'marilari88/twoslash-queries.nvim',
 
-  -- For Windows and Mac, we can open an URL in the browser. For Linux, it may
-  -- not be possible since we maybe in a server which disables GUI.
-  if vim.g.is_win or vim.g.is_mac then
-    -- open URL in browser
-    use({ "tyru/open-browser.vim", event = "VimEnter" })
-  end
+}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+require("lazy").setup(plugins, opts)
